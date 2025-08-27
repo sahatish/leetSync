2a1abbf95293539c59f40ca2e84cbdc2d77ef164
ATISH KUMAR SAH <166246823+sahatish@users.noreply.github.com>
2025-08-27 23:55:36 +0530

    Time: 2 ms (93.78%) | Memory: 42.5 MB (48.91%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -2,33 +2,31 @@ import java.util.*;
 
 class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
+        int[] lastIndex = new int[26];
+        boolean[] seen = new boolean[26];
+        
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
         }
-
-        boolean[] visited = new boolean[26];
-        Deque<Character> stack = new ArrayDeque<>();
-
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']--;
-
-            if (visited[c - 'a']) continue;
-
-            while (!stack.isEmpty() && stack.peekLast() > c && freq[stack.peekLast() - 'a'] > 0) {
-                char removed = stack.removeLast();
-                visited[removed - 'a'] = false;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int idx = c - 'a';
+            
+            if (seen[idx]) continue;
+            
+            while (!stack.isEmpty() && c < stack.peek() && lastIndex[stack.peek() - 'a'] > i) {
+                seen[stack.pop() - 'a'] = false;
             }
-
-            stack.addLast(c);
-            visited[c - 'a'] = true;
+            
+            stack.push(c);
+            seen[idx] = true;
         }
-
-        StringBuilder result = new StringBuilder();
-        for (char c : stack) {
-            result.append(c);
-        }
-
-        return result.toString();
+        
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) sb.append(c);
+        return sb.toString();
     }
 }
