54093dc8d5d6c25693bc417ef05ac3d13a1ab035
ATISH KUMAR SAH <166246823+sahatish@users.noreply.github.com>
2025-10-06 23:03:12 +0530

    Time: 4 ms (30.35%) | Memory: 43.6 MB (35.96%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,32 +1,27 @@
 import java.util.*;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-
-        HashSet <Integer> set = new HashSet<>();
-        for(int n2 : nums2){
-            set.add(n2);
-        }
-
-        ArrayList <Integer> list = new ArrayList<>();
-        for(int n2 : nums1){
-            if(set.contains(n2)){
-                list.add(n2);
-                set.remove(n2);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        HashSet<Integer> set = new HashSet<>();
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
-            int res[] = new int [list.size()];
-            for(int i = 0; i< list.size(); i++){
-               res[i] = list.get(i);
-
-        }
+        
+        int[] res = new int[set.size()];
+        int k = 0;
+        for (int num : set) res[k++] = num;
         return res;
     }
-
-    public static void main(String[] args){
-        Solution sc = new Solution();
-        int [] nums1= {1,2,2,1};
-        int[] nums2 = { 2,2};
-        System.out.println(Arrays.toString(sc.intersection(nums1, nums2)));
-       
-    }
-    }
+}
