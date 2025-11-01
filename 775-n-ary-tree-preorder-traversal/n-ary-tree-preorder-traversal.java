3fd6b00374d22594ffc64285de0f63eadd56324f
ATISH KUMAR SAH <166246823+sahatish@users.noreply.github.com>
2025-11-01 22:20:52 +0530

    Time: 3 ms (21.46%) | Memory: 46.4 MB (7.49%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            result.add(node.val);
+
+            List<Node> children = node.children;
+            if (children != null) {
+                for (int i = children.size() - 1; i >= 0; i--) {
+                    stack.push(children.get(i));
+                }
+            }
+        }
+        return result;
+    }
+}
