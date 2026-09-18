dfab68f875b3ce64875524782b6f4feb4e0a1e32
ATISH KUMAR SAH <166246823+sahatish@users.noreply.github.com>
2026-09-18 23:26:21 +0530

    Time: 3 ms (99.83%) | Memory: 46.6 MB (80.08%) - LeetSync

---
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        boolean[] seen = new boolean[26];
+        int count = 0;
+
+        for(char c : s.toCharArray()){
+            if(!seen[c - 'a']) {
+                seen[c - 'a'] = true;
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
