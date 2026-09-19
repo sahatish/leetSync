5f20086590922cf666b2ad369b709a6c3d89d5ce
ATISH KUMAR SAH <166246823+sahatish@users.noreply.github.com>
2026-09-19 22:44:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (67.11%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,17 +1,49 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return build(s).equals(build(t));
-    }
+        int i = s.length() - 1;
+        int j = t.length() - 1;
 
-    private String build(String str) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : str.toCharArray()) {
-            if (c != '#') {
-                sb.append(c);
-            } else if (sb.length() > 0) {
-                sb.deleteCharAt(sb.length() - 1);
+        while ( i >= 0 || j >= 0){
+            int skips = 0;
+            while(i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skips++;
+                    i--;
+                } else if (skips > 0) {
+                    skips--;
+                    i--;
+                }else {
+                    break;
+                }
             }
+
+            int skipT = 0;
+            while (j >= 0) {
+                if( t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+                }
+
+                if( i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)) {
+                    return false;
+                }
+
+                if((i >= 0) != (j >= 0)) {
+                    return false;
+                }
+
+                i--;
+                j--;
+
+            }
+
+            return true;
         }
-        return sb.toString();
+        
     }
-}
